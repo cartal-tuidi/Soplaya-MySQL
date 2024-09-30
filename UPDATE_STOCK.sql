3181bf58590fa60f98f35eb534a9889b806832dd
cartal-tuidi <lorenzo.carta@tuidi.it>
2024-09-30 12:17:18 +0200

    update soplaya stock procedure

--- UPDATE_STOCK.sql
+++ UPDATE_STOCK.sql
@@ -5,7 +5,7 @@ create
 begin
     declare _max_date date default current_date;
 
-    set _max_date = (select max(start_date) from t_soplaya.stock);
+    set _max_date = (select ifnull(max(start_date),'2999-12-31') from t_soplaya.stock);
     -- #################################################################################
     -- CANCELLAZIONI
     -- #################################################################################
@@ -14,13 +14,16 @@ begin
 
     delete old
     from soplaya.stock old
-             inner join (select p.id as product_id, s.to_be_deleted, s.flg_active
+             inner join (select p.id as product_id, s.to_be_deleted, s.flg_active, s.start_date
                          from t_soplaya.stock s
+                                  inner join soplaya.art_registry ar on s.art_code = ar.art_code
+                                  inner join soplaya.store_registry sr on s.store_code = sr.store_code
                                   inner join soplaya.product p
-                                             on p.product_code = s.product_code) new
+                                             on p.art_registry_id = ar.id and p.store_registry_id = sr.id) new
                         on new.product_id = old.product_id
     where new.to_be_deleted = 1
-       or new.flg_active = 0;
+       or new.flg_active = 0
+       or old.start_date < _max_date;
 
 
     -- #################################################################################
@@ -30,7 +33,13 @@ begin
 /*
 Inserisco le nuove istanze dei record esistenti, in caso di aggiornamento all'interno della stessa giornata, aggiorno il valore della giacenza
  */
-    insert into soplaya.stock(stock, product_id, warehouse_lot, expiry_date, start_date, end_date, flg_active)
+    insert into soplaya.stock(stock,
+                              product_id,
+                              warehouse_lot,
+                              expiry_date,
+                              start_date,
+                              end_date,
+                              flg_active)
 -- Inserire tutti i campi della tabella
     select new.stock,
            p.id,
@@ -40,7 +49,9 @@ Inserisco le nuove istanze dei record esistenti, in caso di aggiornamento all'in
            '2999-12-31' as end_date,
            1            as flg_active
     from t_soplaya.stock new
-             inner join soplaya.product p on p.product_code = new.product_code
+             inner join soplaya.art_registry ar on new.art_code = ar.art_code
+             inner join soplaya.store_registry s on s.store_code = new.store_code
+             inner join soplaya.product p on p.art_registry_id = ar.id and p.store_registry_id = s.id
     where start_date = _max_date
     ON DUPLICATE KEY UPDATE stock       = new.stock,
                             start_date  = current_date,
